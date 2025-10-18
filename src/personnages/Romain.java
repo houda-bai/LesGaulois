eabb7ce435cbb346249a527343c5dfb002a7ba4b
Houda Baidane <houda@baidane.univ-tlse3.fr>
2025-10-18 17:35:22 +0100

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 
 	public String getNom() {
@@ -22,11 +23,23 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert force >0;
+	    int forceAvant = force;
 		force = force - forceCoup;
 		if (force <1) {
 			parler("J'abandonne !");
 		} else {
 			parler("Aie");
 		}
+		assert force < forceAvant;
+		assert isInvariantVerified();
+
+	}
+	private boolean isInvariantVerified() {
+        return force >= 0;  
+    }
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus",6);
+		
 	}
 }
